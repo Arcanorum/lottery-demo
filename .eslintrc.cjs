66289b16b81b385d9f72e25fa4eedeaa6b3a83e4
johnathanf@bitbucket.com <johnathan_fisher93@yahoo.co.uk>
2022-06-26 17:41:35 +0100

    Use 4 space indentation for linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,28 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-  },
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
-  overrides: [
-    {
-      files: ['*.ts'],
-      extends: [
-        'airbnb-base',
-        'airbnb-typescript/base',
-      ],
-      parserOptions: {
-          project: './tsconfig.json',
-      },
+    root: true,
+    env: {
+        browser: true,
+    },
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+    ],
+    overrides: [
+        {
+            files: ['*.ts'],
+            extends: [
+                'airbnb-base',
+                'airbnb-typescript/base',
+            ],
+            parserOptions: {
+                project: './tsconfig.json',
+            },
+            rules: {
+                "@typescript-eslint/indent": ["error", 4],
+            }
+        }
+    ],
+    rules: {
+        "indent": ["error", 4],
     }
-  ]
 };
