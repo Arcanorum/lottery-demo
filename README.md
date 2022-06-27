d90453ff25d175fb0ba7c3fcacfc20fca1eaf084
johnathanf@bitbucket.com <johnathan_fisher93@yahoo.co.uk>
2022-06-27 12:02:40 +0100

    Add webpack production build script

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # lottery-demo
 
-Quickstart:
+Lottery demo game made with Phaser 3 & TypeScript.
 
-Install packages
+User can select either manual or lucky dip mode.
+
+- Manual pick: User can pick their own set of 6 numbers, then proceed to the main game.
+- Lucky dip: 6 numbers are chosen at random and then the main game starts.
+
+6 numbers are then drawn at random, with prizes being given for 3 or more matches with the user's numbers.
+
+After which, they can play again, which restarts the user flow.
+
+## Quickstart
+
+### Install packages
+
 `npm i`
 
-Start dev server
+### Start the local dev server
 `npm run dev`
 
-Open in browser
-`localhost:8080`
+### Open the client in a browser
+http://localhost:8080/
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/game.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        include: path.resolve(__dirname, 'src'),
-        loader: 'ts-loader',
-      },
-      {
-        test: require.resolve('phaser'),
-        loader: 'expose-loader',
-        options: { exposes: { globalName: 'phaser', override: true } },
-      },
-    ],
-  },
-  devServer: {
-    host: 'localhost',
-    port: 8080,
-    open: false,
-    devMiddleware: {
-      publicPath: '/dist/',
+    entry: './src/game.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
     },
-    static: {
-      directory: path.join(__dirname, './'),
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                include: path.resolve(__dirname, 'src'),
+                loader: 'ts-loader',
+            },
+            {
+                test: require.resolve('phaser'),
+                loader: 'expose-loader',
+                options: { exposes: { globalName: 'phaser', override: true } },
+            },
+        ],
     },
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
+    devServer: {
+        host: 'localhost',
+        port: 8080,
+        open: false,
+        devMiddleware: {
+            publicPath: '/dist/',
+        },
+        static: {
+            directory: path.join(__dirname, './'),
+        },
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
 };
